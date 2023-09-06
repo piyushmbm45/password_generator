d91ecd19641b7806f80b7a68dae29b47786dc180
Piyush Jain <piyushmbm45@gmail.com>
2023-09-06 20:53:38 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# password_generator
+React Password Generator
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-white text-xl text-center">Password Generator</h1>
+    </>
+  );
+}
+
+export default App;
